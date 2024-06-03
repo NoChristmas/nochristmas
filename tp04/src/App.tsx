2518d0610ca71b9e457d27e0519d2858faf14b58
jeongho.park1993 <127386254+NoChristmas@users.noreply.github.com>
2024-06-03 14:33:45 +0900

    commit Login Page

--- tp04/src/App.tsx
+++ tp04/src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+
 import './App.css'
 
 const App:React.FC = ()=> {
   
   return (
-    <div className="App">
-      <div>hi~!!</div>
-    </div>
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
   );
 }
 
