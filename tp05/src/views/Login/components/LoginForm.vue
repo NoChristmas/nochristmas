ca72a6313a1ca331f05f580160ef424cad6f2170
jeongho.park1993 <127386254+NoChristmas@users.noreply.github.com>
2024-06-04 18:02:43 +0900

    commit LoginForm

--- tp05/src/views/Login/components/LoginForm.vue
+++ tp05/src/views/Login/components/LoginForm.vue
@@ -1,15 +1,83 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import axios from 'axios';
 
+const usrId = ref('');
+const pwd = ref('');
+
+const login = async () => {
+    //데이터 담는 영역
+    const data = { usr_id : usrId.value, usr_pwd: pwd.value };
+    console.log(JSON.stringify(data));
+    const isAuthenticated = await axios.post('http://localhost:11000/api/v1/login', data);
+    if(isAuthenticated) {
+        alert('로그인 성공!');
+
+    } else {
+        alert('로그인 실패');
+    }
+        
+};
 </script>
 
 <template>
-    <div id="loginComponent">
-        <form>
-            <label>로그인</label>
-        </form>
-    </div>
+    <form @submit.prevent="login" class="login-form">
+        <div class="form-group">
+            <label for="username" class="form-label">ID</label>
+            <input type="text" id="username" v-model="usrId" class="form-input" />
+        </div>
+        <div class="form-group">
+            <label for="password" class="form-label">Password</label>
+            <input type="password" id="password" v-model="pwd" class="form-input" />
+        </div>
+        <button type="submit" class="btn-login">Login</button>
+    </form>
 </template>
 
 <style scoped>
+.login-form {
+  width:300px;
+  margin: 0 auto;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  background-color: #f9f9f9;
+  
+}
 
+.form-group {
+  margin-bottom: 15px;
+  display:flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.form-label {
+  display: block;
+  margin-bottom: 5px;
+}
+
+.form-input {
+  width: 100%;
+  padding: 8px;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+  align-items: center;
+  justify-content: center;
+}
+
+.btn-login {
+  width: 100%;
+  padding: 10px;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 3px;
+  cursor: pointer;
+}
+
+.btn-login:hover {
+  background-color: #0056b3;
+}
 </style>
